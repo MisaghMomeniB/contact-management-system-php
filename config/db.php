ad7968aa84bd440c91fdd9cb299d62143161c94f
MisaghMomeniB <jenabemisagh@gmail.com>
2025-11-17 20:54:23 +0330

    add try catch for connection db

--- config/db.php
+++ config/db.php
@@ -4,3 +4,10 @@ $host = "localhost";
 $user = "root";
 $pass = "Root@12345!";
 $dbname = "contact_management";
+
+try {
+    $pdo = new PDO("mysql:host=$host; dbname=$dbname; charset=utf8", $user, $pass);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Database Connection Failed: " . $e->getMessage());
+}
